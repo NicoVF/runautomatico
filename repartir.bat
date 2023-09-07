3cf3e2a90aeb00901da47b44a1b67ba88a461e45
Nico <nicolas@soybot.com>
2023-09-07 09:24:39 -0300

    Update repartir.bat

--- repartir.bat
+++ repartir.bat
@@ -4,9 +4,9 @@ cd autorepartir
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%"
 
-echo[				    	>> log_%YYYY%_%MM%.log
-echo[			            >> log_%YYYY%_%MM%.log
+echo[				    	>> log_%YYYY%-%MM%.log
+echo[			            >> log_%YYYY%-%MM%.log
 
-python autorepartidor.py    >> log_%YYYY%_%MM%.log
+python autorepartidor.py    >> log_%YYYY%-%MM%.log
 timeout 10
 exit
